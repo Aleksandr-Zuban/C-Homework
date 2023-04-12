b1122aebb48d1be68f97ead8d1ee851c45f5e4cd
Aleksandr-Zuban <130269131+Aleksandr-Zuban@users.noreply.github.com>
2023-04-12 22:50:09 +0300

    add decision for seminar1

--- Seminars/Seminar1_Task4/Program.cs
+++ Seminars/Seminar1_Task4/Program.cs
@@ -10,3 +10,22 @@ Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    if(number1 > number3)
+    {
+    Console.WriteLine("Max: " + number1);
+    }
+    else
+    Console.WriteLine("Max: " + number3);
+}
+else
+{
+    if(number2 > number3)
+    {
+    Console.WriteLine("Max: " + number2);
+    }
+    else
+    Console.WriteLine("Max: " + number3);
+}   
